0e6254c6b5f0911bbcc2fc63fce12d10a3fd4471
Pablo David Encina Acevedo <Pablo.encina@klap.cl>
2026-05-18 20:38:54 -0400

    cambios 36

---
+++ app/test/page.tsx
@@ -0,0 +1,15 @@
+import { createClient } from '@/lib/supabase/server'
+
+export default async function TestPage() {
+  const supabase = await createClient()
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  return (
+    <pre>
+      {JSON.stringify(user, null, 2)}
+    </pre>
+  )
+}
